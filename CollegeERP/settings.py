493d7f54ed49d031e1865a1c2b729a3f068fa202
kawserraihan <kawserraihan2@gmail.com>
2023-09-05 09:38:06 -0700

    settings.py db changed & api updated

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'CollegeERP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -107,7 +107,7 @@ DATABASES = {
         'PORT' : "3306",
         },
     }
-
+"""
 
 
 
